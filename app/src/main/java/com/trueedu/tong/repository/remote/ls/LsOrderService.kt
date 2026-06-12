fc6fc40d053c813d9d85fc85035ab7528036dc23
babo.neo <seirion@mensakorea.org>
2026-06-12 22:49:40 +0900

    LS증권 주문 API 수정 (@Serializable nested body + 성공코드 00000)
    - LsOrderRequest.kt: CSPAT00601InBlock1 @Serializable 요청 body
    - LsOrderService: Map<String,String> → LsOrderRequest
    - LsOrderRepository: 올바른 파라미터, rsp_cd 00000 체크
    - LS 주문 성공코드 0 → 00000 수정

--- app/src/main/java/com/trueedu/tong/repository/remote/ls/LsOrderService.kt
+++ app/src/main/java/com/trueedu/tong/repository/remote/ls/LsOrderService.kt
@@ -1,6 +1,7 @@
 package com.trueedu.tong.repository.remote.ls
 
-import com.trueedu.tong.model.dto.order.LsOrderResponse
+import com.trueedu.tong.model.dto.ls.LsOrderRequest
+import com.trueedu.tong.model.dto.ls.LsOrderResponse
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.HeaderMap
@@ -8,5 +9,8 @@ import retrofit2.http.POST
 
 interface LsOrderService {
     @POST("stock/order")
-    suspend fun order(@HeaderMap headers: Map<String, String>, @Body body: Map<String, String>): Response<LsOrderResponse>
+    suspend fun order(
+        @HeaderMap headers: Map<String, String>,
+        @Body body: LsOrderRequest,
+    ): Response<LsOrderResponse>
 }
